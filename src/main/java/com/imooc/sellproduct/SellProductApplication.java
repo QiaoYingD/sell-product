1fc72ccb21789afdb0906c0b796e6303dfc01a3a
乔应东@qiayingdong.pn <qiaoyingdong.pn@rdunicloud.com>
2019-08-22 14:57:44 +0800

    更新product服务

--- src/main/java/com/imooc/sellproduct/SellProductApplication.java
+++ src/main/java/com/imooc/sellproduct/SellProductApplication.java
@@ -3,11 +3,12 @@ package com.imooc.sellproduct;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
-@MapperScan(basePackages = { "com.imooc.sellproduct.mapper"})
+@MapperScan(basePackages = {"com.imooc.sellproduct.mapper"})
 public class SellProductApplication {
 
     public static void main(String[] args) {
